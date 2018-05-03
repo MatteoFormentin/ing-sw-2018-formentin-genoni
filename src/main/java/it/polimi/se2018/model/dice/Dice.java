68eb80e2def5f06d8ffd68c5625425d6a47f7805
LucaGenoni <36886380+LucaGenoni@users.noreply.github.com>
2018-05-03 15:48:34 +0200

    Aggiunte classi vuote nel Model: Gamebord, dice, ecc

---
+++ src/main/java/it/polimi/se2018/model/dice/Dice.java
@@ -0,0 +1,36 @@
+package it.polimi.se2018.model.dice;
+
+public class Dice {
+    private int value;
+    private DiceColor color;
+
+    /**
+     * Method
+     */
+    public Dice() {
+    }
+
+    public int RollValue() {
+    }
+
+    public int GetValue() {
+
+    }
+
+    public void SetValue(int Value) {
+
+    }
+
+    public int OppositeValue(int Value) {
+
+    }
+
+    public void GetColor() {
+
+    }
+
+    public void SetColor(DiceColor color ) {
+
+    }
+
+}
---
+++ src/main/java/it/polimi/se2018/model/roundTrack/DiceStack.java
@@ -0,0 +1,20 @@
+package it.polimi.se2018.model.roundTrack;
+
+import it.polimi.se2018.model.dice.Dice;
+
+public class DiceStack {
+    private Dice[] dice;
+    private int numberDice;
+
+    public void SetDice(Dice Dice) {
+
+    }
+    public Dice GetDice() {
+
+    }
+
+    public void SetNumberDice(int Number) {
+
+    }
+
+}
