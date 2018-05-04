7ddcc726dc95cfb08da2a5f430ea4569bd89b5fd
LucaGenoni <36886380+LucaGenoni@users.noreply.github.com>
2018-05-04 14:40:41 +0200

    DiceColor fixed

--- src/main/java/it/polimi/se2018/model/dice/dice_color/DiceColor.java
+++ src/main/java/it/polimi/se2018/model/dice/dice_color/DiceColor.java
@@ -1,8 +1,9 @@
-package it.polimi.se2018.model.dice;
+package it.polimi.se2018.model.dice.dice_color;
 
 import java.util.Arrays;
 
 /**
+ * Enum <strong>DiceColor</strong>
  * @author Luca Genoni
  * @version 1.0
  * @since 1.0
@@ -13,7 +14,16 @@ public enum DiceColor {
     Green,
     Blue,
     Purple;
-    private static final int numberOfDiceColors=(int) Arrays.stream(values()).count();
+
+    /**
+     * Class <strong>numberDiceColor</strong>
+     * private static inner class to calculate the number of colors available
+     *
+     * @author Luca Genoni
+     */
+    private static class numberDiceColor {
+        private static final int numberOfColors = (int) Arrays.stream(DiceColor.values()).count();
+    }
 
     /**
      * Method <strong>getDiceColor</strong>
@@ -21,16 +31,15 @@ public enum DiceColor {
      * return the DiceColor given the associated number
      *
      * @param ordinal number int associated with a DiceColor
-     *                @require ordinal belong to domain [0,numberOfDiceColors] numberOfDiceColors=4 if the source code has not changed since version 1.0
      * @return DiceColor associated with the input number, null if there is no match.
+     * @require ordinal belong to domain [0,numberOfDiceColors] numberOfDiceColors=4 if the source code has not changed since version 1.0
      */
-    public static DiceColor getDiceColor(int ordinal){
+    public static DiceColor getDiceColor(int ordinal) {
         /* exchangeable with
          * return Arrays.stream(DiceColor.values()).filter(p -> p.ordinal() == ordinal).findFirst().orElse(null);
          */
-        for ( DiceColor p: values() )
-        {
-            if(p.ordinal() == ordinal) return p;
+        for (DiceColor p : values()) {
+            if (p.ordinal() == ordinal) return p;
         }
         throw new IndexOutOfBoundsException();
     }
@@ -42,10 +51,8 @@ public enum DiceColor {
      *
      * @return int number of color types
      */
-
-    public static int getNumberOfDiceColors(){
-        return numberOfDiceColors;
+    public static int getNumberOfDiceColors() {
+        return numberDiceColor.numberOfColors;
     }
-
-
 }
+
