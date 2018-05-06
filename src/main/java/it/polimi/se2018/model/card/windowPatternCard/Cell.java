3ab79b84d1a26e63df1b41b02396f674b768e6f2
davidemammarella <mammarelladavide@gmail.com>
2018-05-06 03:20:13 +0200

    Updated method in the Cell Class and added new method (setCellColor, setCellValue, getCellColor, getCellValue) Updated method in the WindowPatternCard Class and added new method (checkPreExistingDice)

--- src/main/java/it/polimi/se2018/model/card/windowPatternCard/Cell.java
+++ src/main/java/it/polimi/se2018/model/card/windowPatternCard/Cell.java
@@ -3,26 +3,112 @@ package it.polimi.se2018.model.card.windowPatternCard;
 import it.polimi.se2018.model.dice.Dice;
 import it.polimi.se2018.model.dice.DiceColor;
 
+/**
+ * <strong>Class Cell</strong>
+ * <em>Description</em>
+ * Class that define a Cell of Window Pattern Card.
+ *
+ * @author Davide Mammarella
+ * @version 1.0
+ * @since 1.0
+ */
 
 public class Cell {
 
+    private Dice dice;
+    public int numberRestriction;
+    public DiceColor colorRestriction;
+    private int value;
+    private DiceColor color;
+
     public Cell() {
+
+        Cell() {
+            value=0;
+            color=null;
+        }
+
     }
 
-    private Dice Dice;
-    private boolean Restriction;
-    public int NumberRestriction;
-    public DiceColor ColorRestriction;
-
-    public void SetDice(Dice Dice) {
+    /**
+     * Method <strong>setCellValue</strong>
+     * <em>Description</em>
+     * Set a value for the cell, based on the dice added to it.
+     *
+     * @param value to set on the cell
+     */
+    public void setCellValue(int value) {
+        this.value = value;
     }
 
-    public Dice GetDice() {
-        return null;
+    /**
+     * Method <strong>setCellColor</strong>
+     * <em>Description</em>
+     * Set a color for the cell, based on the dice added to it.
+     *
+     * @param color to set on the cell
+     */
+    public void setCellColor(DiceColor color) {
+        this.color = color;
     }
 
-    public boolean CheckRestriction() {
-        return false;
+    /**
+     * Method <strong>getCellValue</strong>
+     * <em>Description</em>
+     * Get the value of the dice on the cell.
+     *
+     */
+    public int getCellValue() {
+        return value;
+    }
+    /**
+     * Method <strong>getCellColor</strong>
+     * <em>Description</em>
+     * Get the color of the dice on the cell.
+     *
+     */
+    public DiceColor getCellColor() {
+        return color;
+    }
+
+    /**
+     * Method <strong>getCellDice</strong>
+     *
+     * @return the dice
+     */
+    public Dice getCellDice() { return this.dice; }
+
+    /**
+     * Method <strong>checkCellRestriction</strong>
+     * <em>Description</em>
+     * Check if the dice is allowed based on the color or value restriction of the cell.
+     *
+     * @return true if the restriction is verified, false if the restriction is not verified.
+     */
+    public boolean checkCellRestriction(DiceColor diceColor, int diceNumber) {
+        if(numberRestriction==diceNumber || colorRestriction==diceColor){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Method <strong>insertDice</strong>
+     * <em>Description</em>
+     * Insert the dice in the cell if the restriction of the cell are respected.
+     *
+     */
+    public void insertDice(Dice dice) {
+        value = dice.getValue();
+        color = dice.getColor();
+
+        if(checkCellRestriction(color,value)==true){
+            System.out.print("You can't insert the Dice, it does not respect the restriction");
+        } else {
+            setCellColor(color);
+            setCellValue(value);
+        }
     }
 
 }
--- src/main/java/it/polimi/se2018/model/card/windowPatternCard/WindowPatternCard.java
+++ src/main/java/it/polimi/se2018/model/card/windowPatternCard/WindowPatternCard.java
@@ -2,52 +2,181 @@ package it.polimi.se2018.model.card.windowPatternCard;
 
 import it.polimi.se2018.model.dice.Dice;
 import it.polimi.se2018.model.dice.DiceColor;
+/**
+ * <strong>Class WindowPatternCard</strong>
+ * <em>Description</em>
+ * Class that define the Window Pattern Card.
+ *
+ * @author Davide Mammarella
+ * @version 1.0
+ * @since 1.0
+ */
 
 public class WindowPatternCard {
 
-    public WindowPatternCard(){
-    }
-
     private int ID;
-    private String Name;
-    private int Level;
-    private Cell[][] Matrix; //[4][5] inizializzare nel costruttore
+    private String name;
+    private int level;
+    private int cellValue;
+    private DiceColor cellColor;
+    private Cell[][] Matrix;
 
-    public String GetName() {
-        return null;
+    public WindowPatternCard() {
+        Matrix = new Cell[5][4];
     }
 
-    public void SetName(String Name) {
+    /**
+     * Method <strong>getName</strong>
+     * <em>Description</em>
+     * Get the name of the Window Pattern Card.
+     */
+    public String getName() {
+        return name;
     }
 
-    public int GetLevel() {
-        return 0;
+    /**
+     * Method <strong>setName</strong>
+     * <em>Description</em>
+     * Set a name for the Window Pattern Card.
+     *
+     * @param name assigned to the Window Pattern Card
+     */
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public void SetLevel(int Level) {
+    /**
+     * Method <strong>getLevel</strong>
+     * <em>Description</em>
+     * Get the level of the Window Pattern Card.
+     */
+    public int getLevel() {
+        return level;
     }
 
-    public Cell GetCell(int Line, int Column) {
-        return null;
+    /**
+     * Method <strong>setLevel</strong>
+     * <em>Description</em>
+     * Set the level of the Window Pattern Card.
+     *
+     * @param level assigned to the Window Pattern Card
+     */
+    public void setLevel(int level) {
+        this.level = level;
     }
 
-    public boolean CheckColorRestriction(int Line, int Column, DiceColor Color) {
+    /**
+     * Method <strong>getCell</strong>
+     * <em>Description</em>
+     * Get a cell of the Window Pattern Card.
+     *
+     * @param line   used as a cell reference
+     * @param column used as a cell reference
+     * @return the cell referenced to line and column
+     */
+    public Cell getCell(int line, int column) {
+        return Matrix[line][column];
+    }
+
+    /**
+     * Method <strong>checkPreExistingDice</strong>
+     * <em>Description</em>
+     * Check if there is a dice in a selected cell of the window pattern card.
+     *
+     * @return true if the restriction is verified, false if the restriction is not verified
+     */
+    public boolean checkPreExistingDice(int line, int column) {
+        cellColor = Matrix[line][column].getCellColor();
+        cellValue = Matrix[line][column].getCellValue();
+
+        if (cellColor != null && cellValue != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Method <strong>checkMatrixAdjacentRestriction</strong>
+     * <em>Description</em>
+     * Check if the dice is allowed based on the restriction imposed from the adjacent cell.
+     *
+     * @return true if the restriction is verified, false if the restriction is not verified
+     */
+    public boolean checkMatrixAdjacentRestriction(int line, int column) {
+        if (Matrix[line][column-1].getCellColor()!=null && Matrix[line][column-1].getCellValue()!=0){
+            return true;
+        } else if (Matrix[line][column+1].getCellColor()!=null && Matrix[line][column+1].getCellValue()!=0){
+            return true;
+        } else if (Matrix[line-1][column].getCellColor()!=null && Matrix[line-1][column].getCellValue()!=0){
+            return true;
+        } else if (Matrix[line-1][column-1].getCellColor()!=null && Matrix[line-1][column-1].getCellValue()!=0){
+            return true;
+        } else if(Matrix[line-1][column+1].getCellColor()!=null && Matrix[line-1][column+1].getCellValue()!=0){
+            return true;
+        }else if (Matrix[line+1][column].getCellColor()!=null && Matrix[line+1][column].getCellValue()!=0){
+            return true;
+        } else if (Matrix[line+1][column-1].getCellColor()!=null && Matrix[line+1][column-1].getCellValue()!=0){
+            return true;
+        } else if(Matrix[line+1][column+1].getCellColor()!=null && Matrix[line+1][column+1].getCellValue()!=0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Method <strong>checkMatrixColorRestriction</strong>
+     * <em>Description</em>
+     * Check if the dice is allowed based on the color restriction of the matrix.
+     *
+     * @return true if the restriction is verified, false if the restriction is not verified
+     */
+    public boolean checkMatrixColorRestriction(int line, int column, DiceColor diceColor) {
+        if(diceColor == Matrix[line][column-1].getCellColor()){
+            return true;
+        }
+        else if (diceColor == Matrix[line][column+1].getCellColor()){
+            return true;
+        }
+        else if (diceColor == Matrix[line-1][column].getCellColor()){
+            return true;
+        else if (diceColor == Matrix[line+1][column].getCellColor()){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Method <strong>checkMatrixValueRestriction</strong>
+     * <em>Description</em>
+     * Check if the dice is allowed based on the value restriction of the matrix.
+     *
+     * @return true if the restriction is verified, false if the restriction is not verified
+     */
+    public boolean checkMatrixValueRestriction(int line, int column, int diceValue) {
+        if(diceValue == Matrix[line][column-1].getCellValue()){
+            return true;
+        }
+        else if (diceValue == Matrix[line][column+1].getCellValue()){
+            return true;
+        }
+        else if (diceValue == Matrix[line-1][column].getCellValue()){
+            return true;
+        }
+        else if (diceValue == Matrix[line+1][column].getCellValue()){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean insertDice(int line, int column, Dice Dice) {
         return false;
     }
 
-    public boolean CheckValueRestriction(int Line, int Column, int Value) {
-        return false;
-    }
-
-    public boolean CheckAdjacentRestriction(int Line, int Column) {
-        return false;
-    }
-
-    public boolean InsertDice(int Line, int Column, Dice Dice) {
-        return false;
-    }
-
-    public boolean InsertDice(int Line, int Column, Dice Dice, boolean ColorR, boolean AdjacentR, boolean ValueR) {
+    public boolean insertDice(int line, int column, Dice Dice, boolean colorR, boolean adjacentR, boolean valueR) {
         return false;
     }
 
