d50e22bc4405c4cceaf3d9a3003e11b85953a765
davidemammarella <mammarelladavide@gmail.com>
2018-05-18 11:39:06 +0200

    Deleted NetworkException, implementend starter for one server or client.

--- src/main/java/it/polimi/se2018/network/StartClient.java
+++ src/main/java/it/polimi/se2018/network/StartClient.java
@@ -1,4 +1,15 @@
 package it.polimi.se2018.network;
 
+import it.polimi.se2018.network.client.*;
+
+/**
+ * Class used to instantiate ONE client.
+ *
+ * @author Davide Mammarella
+ */
+
 public class StartClient {
+    public static void main(String[] args) {
+        Client.main(args);
+    }
 }
--- src/main/java/it/polimi/se2018/network/StartServer.java
+++ src/main/java/it/polimi/se2018/network/StartServer.java
@@ -1,4 +1,15 @@
 package it.polimi.se2018.network;
 
+import it.polimi.se2018.network.server.*;
+
+/**
+ * Class used to instantiate ONE server.
+ *
+ * @author Davide Mammarella
+ */
+
 public class StartServer {
+    public static void main(String[] args) {
+        Server.main(args);
+    }
 }
