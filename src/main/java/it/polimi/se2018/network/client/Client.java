a3e1ada5f531afc32c985010b018bb1a5cf31edc
davidemammarella <mammarelladavide@gmail.com>
2018-05-18 21:37:27 +0200

    Implemented Client with the Adapter Pattern.

--- src/main/java/it/polimi/se2018/network/client/Client.java
+++ src/main/java/it/polimi/se2018/network/client/Client.java
@@ -1,4 +1,53 @@
 package it.polimi.se2018.network.client;
 
-public class Client {
+/**
+ * Class that define the client side of the game.
+ *
+ * @author Davide Mammarella
+ */
+public class Client implements ClientController{
+
+    // Classe che rappresenta il client selezionato
+    private AbstractClient client;
+
+    // Interfaccia utilizzata per inviare eventi a CLI o GUI (a seconda del tipo di interfaccia utilizzata per il client)
+    private ClientController ui;
+
+    // Indirizzo su cui le comunicazioni sono aperte a lato server
+    private static final String HOST = "localhost";
+
+    // Porta utilizzata per la comunicazione socket
+    private static final int SERVER_SOCKET_PORT = 61803;
+
+    // Porta utilizzata per la comunicazione RMI
+    //private static final int SERVER_RMI_PORT = ;
+
+    // Flag utile per determinare lo stato della connessione
+    private boolean isLogged;
+
+    // Nome del giocatore corrente
+    private String username;
+
+    // Turno della partita (da 1 a 9)
+    private int turn;
+
+    public Client(ClientController ui){
+        this.ui = ui;
+        username = "anonymous";
+        isLogged = false;
+
+        // COSE (RISORSE, DADI, ECC) DEI GIOCATORI (AGGIORNATE ALL'ULTIMO AGGIORNAMENTO RICEVUTO DAL SERVER
+
+        this.turn = 0;
+
+        //responseMap = new HashMap<>();
+       //loadResponses();
+    }
+
+    // handle response gestirà la risposta ricevuta dal Server ed invocherà il metodo associatogli nella MAPPA DELLE RESPONSE
+
+    // METODI CHE SI SCATENANO QUANDO IL SERVER NOTIFICA UN PRECISO EVENTO
+
+    //METODO INVOCATO DAL SERVER QUANDO L'AZIONE RICHIESTA DAL GIOCATORE VIENE ACCETTATA
+
 }
